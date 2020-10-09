7ba8d63fdc4a873b2bb8c3e19b32d8b795925157
Patel210 <pranavpatel2101997@gmail.com>
2020-10-09 18:37:53 +0530

    Modified: Ability to find cheapest hotel with different weekday and weekends rate

--- src/com/capgemini/hotelreservation/HotelReservation.java
+++ src/com/capgemini/hotelreservation/HotelReservation.java
@@ -1,7 +1,14 @@
 package com.capgemini.hotelreservation;
 
+import java.text.ParseException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 public class HotelReservation {
 
@@ -14,12 +21,63 @@ public class HotelReservation {
 	/**
 	 * @param hotelName
 	 * @param weekdayRate
-	 * @param weekendRate
-	 *  Add Hotel in the system
+	 * @param weekendRate Add Hotel in the system
 	 */
 	public void addHotelToSystem(String hotelName, Integer weekdayRate, Integer weekendRate) {
 		Hotel hotel = new Hotel(hotelName, weekdayRate, weekendRate);
 		availableHotels.put(hotelName, hotel);
 		System.out.println("Hotel added  successfully!");
+	}
+
+	/**
+	 * @param startDate
+	 * @param endDate
+	 * @return array containing no. of week days and no. of weekend days in format
+	 *         array[0]=weekdays and array[1] = weekend days
+	 * @throws ParseException
+	 */
+	private int[] getWeekDaysAndWeekendDays(String startDate, String endDate) throws ParseException {
+		LocalDate endDateOfReservation = null, startDateOfReservation = null;
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		startDateOfReservation = LocalDate.parse(startDate, dtf);
+		endDateOfReservation = LocalDate.parse(endDate, dtf);
+
+		LocalDate tempDate = startDateOfReservation;
+		int noOfDays = (int) ChronoUnit.DAYS.between(startDateOfReservation, endDateOfReservation) + 1;
+		int noOfWeekendDays = 0;
+		for (int i = 0; i < noOfDays; i++) {
+			DayOfWeek day = DayOfWeek.of(tempDate.get(ChronoField.DAY_OF_WEEK));
+			switch (day) {
+			case SATURDAY:
+				noOfWeekendDays++;
+				break;
+			case SUNDAY:
+				noOfWeekendDays++;
+				break;
+			}
+			tempDate = tempDate.plusDays(1);
+		}
+		return new int[] { noOfDays - noOfWeekendDays, noOfWeekendDays };
+	}
+
+	/**
+	 * @param startDate
+	 * @param endDate
+	 * @throws ParseException View the cheapest hotel if present
+	 */
+	public void findCheapestHotel(String startDate, String endDate) throws ParseException {
+		int[] countOfDiffDays = getWeekDaysAndWeekendDays(startDate, endDate);
+
+		Function<Hotel, Integer> toGetRates = hotel -> Integer.sum(hotel.getWeekdayRate() * countOfDiffDays[0],
+				hotel.getWeekendRate() * countOfDiffDays[1]);
+		if (availableHotels.size() != 0) {
+			int cheapestRate = availableHotels.entrySet().stream().map(entry -> toGetRates.apply(entry.getValue()))
+					.min((i, j) -> i.compareTo(j)).get();
+			availableHotels.entrySet().stream().filter(entry -> toGetRates.apply(entry.getValue()) == cheapestRate)
+					.forEach(entry -> System.out
+							.println("Cheapest Hotel: " + entry.getKey() + " Total rates: $" + cheapestRate));
+		} else {
+			System.out.println("Sorry! No Hotel is available in the system");
+		}
 	}
 }
--- src/com/capgemini/hotelreservation/HotelReservationUtility.java
+++ src/com/capgemini/hotelreservation/HotelReservationUtility.java
@@ -1,5 +1,8 @@
 package com.capgemini.hotelreservation;
 
+import java.text.ParseException;
+import java.util.Scanner;
+
 public class HotelReservationUtility {
 
 	public static void main(String[] args) {
@@ -8,5 +11,16 @@ public class HotelReservationUtility {
 		hotelReservation.addHotelToSystem("Lakewood", 110, 90);
 		hotelReservation.addHotelToSystem("Bridgewood", 150, 50);
 		hotelReservation.addHotelToSystem("Ridgewood", 220, 150);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the start date(ddMMMyyyy): ");
+		String startDate = sc .next();
+		System.out.println("Enter the end date(ddMMMyyyy): ");
+		String endDate = sc .next();
+		try {
+			hotelReservation.findCheapestHotel(startDate, endDate);
+		} catch (ParseException e) {
+			System.out.println("Invalid date format! Correct format: ddMMMyyyy");
+		}
 	}
 }
